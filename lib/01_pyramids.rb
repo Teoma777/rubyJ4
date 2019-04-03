d1938a66b7c5e21c73c8fb63c557884e7a350e99
Teoma777 <groux.thomas.uniscite@gmail.com>
2019-04-03 19:00:00 +0200

    full_pyramid vraiment ok

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -24,14 +24,16 @@ def full_pyramid
 nombre = ask_nombre	
 i = nombre.to_i - 1
 j = 1
+k = 0
 n = 1
 while n <= nombre.to_i
-puts [" "].cycle(i).to_a.join('') + ["#"].cycle(j).to_a.join('') +  ["#"].cycle(j).to_a.join('')  
+puts [" "].cycle(i).to_a.join('') + ["#"].cycle(j).to_a.join('') +  ["#"].cycle(k).to_a.join('')  
 
  [" "].cycle(i).to_a.join('') 
 i -= 1
 n += 1
 j += 1
+k += 1
 end
 end
 
