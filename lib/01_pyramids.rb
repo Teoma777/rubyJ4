e4c629dc02ed008fcd1f254ffcd0b80dc51f604f
Teoma777 <groux.thomas.uniscite@gmail.com>
2019-04-03 18:42:21 +0200

    half_pyramid OK

---
+++ lib/01_pyramids.rb
@@ -0,0 +1,28 @@
+	
+def ask_nombre
+	
+puts "Choissisez un nombre entre 1 & 25 "
+print "> "
+nombre = gets.chomp
+end 
+
+def half_pyramid
+nombre = ask_nombre	
+i = nombre.to_i - 1
+j = 1
+
+n = 1
+while n <= nombre.to_i
+puts [" "].cycle(i).to_a.join('') + ["#"].cycle(j).to_a.join('') 
+
+ [" "].cycle(i).to_a.join('') 
+
+
+i -= 1
+n += 1
+j += 1
+
+end
+end
+
+half_pyramid
