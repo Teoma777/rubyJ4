682e355983b31be3d6eff2fd46dd8b1c8b5870b4
Teoma777 <groux.thomas.uniscite@gmail.com>
2019-04-03 19:41:03 +0200

    wtf_pyramid ok

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -37,4 +37,31 @@ k += 1
 end
 end
 
-full_pyramid
+def wtf_pyramid
+
+nombre = ask_nombre	
+i = nombre.to_i - 1
+j = 1
+k = 0
+n = 1
+while n <= nombre.to_i/2
+puts [" "].cycle(i).to_a.join('') + ["#"].cycle(j).to_a.join('') +  ["#"].cycle(k).to_a.join('')  
+
+n += 1 # maximum d'étages
+
+i -= 1 # espaces au début
+j += 1 # blocs
+k += 1 # blocs de fin
+end
+while n != nombre.to_i + 1
+	
+
+puts [" "].cycle(i).to_a.join('') + ["#"].cycle(j).to_a.join('') +  ["#"].cycle(k).to_a.join('')
+
+i += 1
+n += 1
+j -= 1
+k -= 1
+end
+end
+wtf_pyramid
