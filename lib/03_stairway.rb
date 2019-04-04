f0ebe604aadb53e6266c0ece1031f4b75c949a0d
Teoma777 <groux.thomas.uniscite@gmail.com>
2019-04-04 15:00:06 +0200

    quasiment fini, reste phrase de victoire a faire

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -1,6 +1,52 @@
-def dé 
-resultat = rand 7
-puts resultat
+$i = 0 #score
+$t = 0 #tours
+
+def welcome
+	puts "Bienvenue dans ce jeu bla bla bla "
+
+end
+
+
+
+
+
+def lancedé
+	resultat = (rand 6 ) + 1
+	puts  "votre lancer de dé :" +  "#{resultat}"
+	if resultat.to_i < 2 
+
+	$i -= 1
+
+	puts " *MOTUS_black_ball.mp3 * Vous reculez d'une case " + "votre score est de #{$i}." + " Vous etes au tour #{$t}"  
+	
+
+
+
+elsif resultat.to_i > 4 
+ 
+	$i += 1
+
+	puts "Félicitations ! Vous avancez d'une case , votre score est de #{$i}. "  + " Vous etes au tour #{$t} "
+
+
+else 
+	puts  "Vous n'avancez pas ce tout ci, votre score est de #{$i}. " + " Vous etes au tour #{$t}. "
+
+
+end
 end 
 
-dé
+
+
+def jeu
+
+
+
+while $i < 10 do
+ lancedé
+ $t += 1
+
+end
+end
+
+jeu 
