819ea486cb4fbdf95bbec8bf25b83f63c4b7bf15
Teoma777 <groux.thomas.uniscite@gmail.com>
2019-04-04 15:41:20 +0200

    100 fois OK

--- lib/03_stairway.rb
+++ lib/03_stairway.rb
@@ -1,5 +1,6 @@
 $s = 0 #score
 $t = 0 #tours
+$times = [] #liste des scores
 
 def welcome
 	puts "Bienvenue dans ce jeu bla bla bla "
@@ -27,10 +28,14 @@ end
 
 def gg
 	puts "BRAVO TU AS GAGNE TES TROP FORT !!! " 
+		$times << $t-1
+		puts "tes scores : #{$times}"
+		$s = 0 #reset des variables
+        $t = 0 #reset des variables
 end
 
 def half_pyramid
-nombre = $s
+nombre = $s #progression du joueur
 i = nombre.to_i - 1
 j = 1
 n = 1
@@ -42,7 +47,6 @@ j += 1
 end
 end
 
-
 def jeu
 	while 	$s < 10 
  	lancedé
@@ -51,4 +55,10 @@ end
 gg
 end
 
-welcome 
+def average_finish_time
+	100.times do jeu 
+end
+	puts "Ton score moyen est de #{$times.sum / $times.size.to_f } tours... Tu n'es pas très fort !" 
+end
+
+average_finish_time
